71ed0898b6a15ee9547a54f906ae5243c0dbe24d
Nicolas Modrzyk <hellonico@gmail.com>
2023-05-09 14:13:06 +0900

    wildcard for package.sh

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-jpackage  --type dmg --java-options '--enable-preview' --name Booth  -i target -n booth --main-class origami.booth.Main --main-jar booth-0.9.1.jar
+mvn package
+jpackage  --type dmg --java-options '--enable-preview' --name Booth  -i target -n booth --main-class origami.booth.Main --main-jar booth-*.jar
